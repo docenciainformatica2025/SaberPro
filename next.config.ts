25b1bc7cadfc95a393dcd473d000464842a27683
SaberPro Deploy <deploy@saberpro.com>
2025-12-20 22:09:21 -0500

    fix(config): remove removed Sentry options (hideSourceMaps)

--- next.config.ts
+++ next.config.ts
@@ -60,7 +60,4 @@ export default withSentryConfig(nextConfig, {
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
   tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
 });
